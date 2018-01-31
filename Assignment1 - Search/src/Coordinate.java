cc1288698a06259a7d7e6a0638294bd69046c535
fridab <frida.bornfors@gmail.com>
2018-01-31 09:47:57 +0100

    Add Coordinate class

---
+++ Assignment1 - Search/src/Coordinate.java
@@ -0,0 +1,45 @@
+
+public class Coordinate {
+	private int row;
+	private char col;
+	
+	/**
+	 * X = column
+	 * Y = row
+	 * @param XY
+	 */
+	public Coordinate(String XY) {
+			col = Character.toLowerCase(XY.charAt(0));
+			row = XY.charAt(1);
+	}
+
+	public int getRow() {
+		return row;
+	}
+	
+	public int getCol(){
+		switch(col){
+		case 'a':
+		return 0;
+		case 'b':
+		return 1;
+		case 'c':
+		return 2;
+		case 'd':
+		return 3;
+		case 'e':
+		return 4;
+		case 'f':
+		return 5;
+		case 'g':
+		return 6;
+		case 'h':
+		return 7;
+		default:
+			return -1;
+			
+		}
+			
+		
+	}
+}
